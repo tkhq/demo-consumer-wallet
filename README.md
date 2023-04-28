7ea901e6e605dfdc79df8224799618e6b006b90f
Keyan Zhang <keyan@turnkey.io>
2023-04-27 18:58:44 -0700

    Initial expo project setup

---
+++ README.md
@@ -0,0 +1,14 @@
+# Turnkey Demo: Consumer Wallet
+
+## Introduction
+
+This repository features a minimal consumer wallet app powered by Turnkey. Behind the scenes, it uses [`@turnkey/ethers`](https://github.com/tkhq/sdk/tree/main/packages/ethers) for signing and WalletConnect (v1) for accessing dapps.
+
+## Getting started
+
+```bash
+$ corepack enable # Updates npm for the local project
+
+$ npm install
+$ npm start
+```
---
+++ app.config.js
@@ -0,0 +1,43 @@
+// @ts-check
+
+const dotenv = require("dotenv");
+
+const envResult = dotenv.config();
+
+if (envResult.error) {
+  console.error(envResult.error);
+  throw envResult.error;
+}
+
+/**
+ * @type {import("@expo/config").ExpoConfig}
+ */
+const config = {
+  name: "turnkey-demo-consumer-wallet",
+  slug: "turnkey-demo-consumer-wallet",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#ffffff",
+    },
+  },
+  web: {
+    favicon: "./assets/favicon.png",
+  },
+  extra: envResult.parsed,
+};
+
+module.exports = config;
